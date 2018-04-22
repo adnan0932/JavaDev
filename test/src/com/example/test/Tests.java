7d7ded6d2d1592b9081c44c082fddadf16451019
adnan0932 <adnan.cse13@gmail.com>
2018-04-22 13:10:37 +0200

    main
    main method

---
+++ test/src/com/example/test/Tests.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+public class Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
