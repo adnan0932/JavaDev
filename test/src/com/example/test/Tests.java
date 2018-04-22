2c317bdbf6244b70b50e313ee99112e1ccf43f02
adnan0932 <adnan.cse13@gmail.com>
2018-04-22 15:28:50 +0200

    # WARNING: head commit changed in the meantime
    main

--- test/src/com/example/test/Tests.java
+++ test/src/com/example/test/Tests.java
@@ -1,10 +1,23 @@
 package com.example.test;
 
+import java.util.ArrayList;
+
 public class Tests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		ArrayList<String> list = new  ArrayList<>();
+		list.add("adnan");
+		list.add("kawsar");
+		list.add("masao");
+		list.add("josa");
+		
+		for (int i = 0; i< list.size();i++)
+		{
+			System.out.println(list.get(i));
+		}
 
 	}
 
